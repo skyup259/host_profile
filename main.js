4696d31a22e5d0a33f9193ae7129a6a96cf6d9b7
skyup259 <saurabh.kumar259@gmail.com>
2020-08-28 16:08:10 +0530

    changed image path

--- main.js
+++ main.js
@@ -1138,47 +1138,47 @@ class SlidingcardComponent {
     constructor() {
         this.data = [
             {
-                event: '../../../assets/book1.jpg',
+                event: 'assets/book1.jpg',
                 caption: '07/14/20 - 07/19/20',
                 descripation: 'Meluha is a near perfect empire, created many centuries earlier by Lord Ram, one of the greatest Hindu kings that ever lived. However, the once proud empire and its Suryavanshi rulers face severe crisis as its primary river, Saraswati, is slowly drying to extinction.'
             },
             {
-                event: '../../../assets/book2.jpg',
+                event: 'assets/book2.jpg',
                 caption: '07/22/20 - 08/3/20',
                 descripation: ' The story takes place in the imaginary land of Meluha and narrates how the inhabitants of that land are saved from their wars by a nomad named Shiva.'
             },
             {
-                event: '../../../assets/book3.jpg',
+                event: 'assets/book3.jpg',
                 caption: '08/12/20 - 08/30/20',
                 descripation: 'The battle rages on and Shiva travels to the land of Pariha to consult with Vayuputras, a legendary tribe. By the time he returns, the war has ended with Sati, his wife, being murdered.'
             },
             {
-                event: '../../../assets/book2.jpg',
+                event: 'assets/book2.jpg',
                 caption: '07/22/20 - 08/3/20',
                 descripation: ' The story takes place in the imaginary land of Meluha and narrates how the inhabitants of that land are saved from their wars by a nomad named Shiva.'
             },
             {
-                event: '../../../assets/book3.jpg',
+                event: 'assets/book3.jpg',
                 caption: '07/22/20 - 08/3/20',
                 descripation: 'The battle rages on and Shiva travels to the land of Pariha to consult with Vayuputras, a legendary tribe. By the time he returns, the war has ended with Sati, his wife, being murdered.'
             },
             {
-                event: '../../../assets/book3.jpg',
+                event: 'assets/book3.jpg',
                 caption: '07/22/20 - 08/3/20',
                 descripation: 'The battle rages on and Shiva travels to the land of Pariha to consult with Vayuputras, a legendary tribe. By the time he returns, the war has ended with Sati, his wife, being murdered.'
             },
             {
-                event: '../../../assets/book3.jpg',
+                event: 'assets/book3.jpg',
                 caption: '07/22/20 - 08/3/20',
                 descripation: 'The battle rages on and Shiva travels to the land of Pariha to consult with Vayuputras, a legendary tribe. By the time he returns, the war has ended with Sati, his wife, being murdered.'
             },
             {
-                event: '../../../assets/book1.jpg',
+                event: 'assets/book1.jpg',
                 caption: '07/14/20 - 07/19/20',
                 descripation: 'Meluha is a near perfect empire, created many centuries earlier by Lord Ram, one of the greatest Hindu kings that ever lived. However, the once proud empire and its Suryavanshi rulers face severe crisis as its primary river, Saraswati, is slowly drying to extinction.'
             },
             {
-                event: '../../../assets/book2.jpg',
+                event: 'assets/book2.jpg',
                 caption: '07/22/20 - 08/3/20',
                 descripation: ' The story takes place in the imaginary land of Meluha and narrates how the inhabitants of that land are saved from their wars by a nomad named Shiva.'
             }
